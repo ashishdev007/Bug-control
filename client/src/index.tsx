bf0e3923245da7a7a27c90e4d76ccbc9ebb46324
Ashish Dev <ashishdevoffical@gmail.com>
2020-02-18 22:24:15 -0600

    redux setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducers from './reducers/index';
+
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ client/src/reducers/index.tsx
@@ -0,0 +1,5 @@
+import { createStore, combineReducers } from 'redux';
+
+const reducers = combineReducers({});
+
+export default reducers;
